9092a86c1c3957c51e92dce29cf405bec9780057
PISTOLCUPCAKES <rpelton16@gmail.com>
2021-03-26 12:29:16 -0400

    updated env file and improved env setup

--- qBittorrent/qbittorrent_copy_on_complete.sh
+++ qBittorrent/qbittorrent_copy_on_complete.sh
@@ -1,30 +1,41 @@
-############
-#  Ubuntu  #
-############
-# %N	ubuntu-18.04.5-desktop-amd64.iso
-# %F	/data/2-complete/arr/ubuntu-18.04.5-desktop-amd64.iso
-# %R	/data/2-complete/arr/ubuntu-18.04.5-desktop-amd64.iso
-# %D	/data/2-complete/arr
-# %C	1
-# %Z	2193522688
-# %T	https://torrent.ubuntu.com/announce
-# %I	94a315e2cf8015b2f635d79aab592e6db557d5ea
+#!/bin/bash
 
-############
-#  Mayans  #
-############
-# %N	Mayans.M.C.S03E03.Overreaching.Dont.Pay.1080p.AMZN.WEBRip.DDP5.1.x264-NTb[rartv]
-# %F	/data/2-complete/arr/Mayans.M.C.S03E03.Overreaching.Dont.Pay.1080p.AMZN.WEBRip.DDP5.1.x264-NTb[rarbg]
-# %R	/data/2-complete/arr/Mayans.M.C.S03E03.Overreaching.Dont.Pay.1080p.AMZN.WEBRip.DDP5.1.x264-NTb[rarbg]
-# %D	/data/2-complete/arr
-# %C	3
-# %Z	4164683787
-# %T	udp://9.rarbg.to:2760
-# %I	d0bdb9e003eb5333a3b0c4ad51adecf10524ccbd
+set -e
 
-#!/bin/sh
+# ------------------------------------------------------------------------------
+# Script        | qbittorrent_copy_on_complete.sh
+# Description   | Script to copy a completed torrent to another directory
+#                   and resume the torrent
+# qbittorrent command: qbittorrent_copy_on_complete.sh '%R' '%D'
+# ------------------------------------------------------------------------------
 
-save_path=%D
+####################################
+#                                  #
+####################################
+
+####################################
+#           ENV SETUP              #
+####################################
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
+
+# shellcheck source=qbittorrent.env
+. "${SCRIPT_DIR}"/qbittorrent.env
+
+readonly TORRENT_PATH="$1"
+readonly SAVE_PATH=%D
+
+####################################
+#         VERIFY OPTIONS           #
+####################################
+
+
+####################################
+#              COPY                #
+####################################
+
+cp -r "${TORRENT_PATH}" "${COPY_TO_DIR}"
+
 
 
 category="$(basename $1)"
